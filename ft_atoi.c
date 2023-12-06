b08da12b90e346bcd425f4e525f617f87cda958e
Abdelwadoud Sayad <asayad@e1r8p5.1337.ma>
2023-12-06 18:31:07 +0100

    refined

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,29 +6,25 @@
 /*   By: asayad <asayad@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 16:17:10 by asayad            #+#    #+#             */
-/*   Updated: 2023/12/06 10:32:40 by asayad           ###   ########.fr       */
+/*   Updated: 2023/12/06 18:30:04 by asayad           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
-static int	next(int j, int sign, int num)
+static int	check_overflow(int s)
 {
-	if (j >= 19 && sign == 1)
-		return (-1);
-	if (j >= 19 && sign == -1)
+	if (s < 0)
 		return (0);
-	num = num * sign;
-	return (num);
+	return (-1);
 }
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	num;
-	int	j;
+	int					sign;
+	unsigned long long	num;
 
-	j = 0;
 	num = 0;
 	sign = 1;
 	while ((*str >= 9 && *str <= 13) || *str == 32)
@@ -43,10 +39,16 @@ int	ft_atoi(const char *str)
 		str++;
 	while (*str >= 48 && *str <= 57)
 	{
-		j++;
-		num = num * 10 + (*str - 48);
+		if ((num * 10 + (*str - 48)) > 9223372036854775807)
+			return (check_overflow(sign));
+		num = (num * 10) + (*str - 48);
 		str++;
 	}
-	return (next(j, sign, num));
 	return (num * sign);
 }
+// #include <limits.h>
+// int main()
+// {
+// 	printf("%d\n", atoi("9223372036854775810"));
+// 	printf("%d\n", ft_atoi("9223372036854775810"));
+// }
